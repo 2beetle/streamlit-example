7d88315768f9a66980619c8aa22d83de8a06cb93
2beetle <74488739+2beetle@users.noreply.github.com>
2024-01-25 16:34:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,17 @@
-import altair as alt
-import numpy as np
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
 
-"""
-# Welcome to Streamlit!
+if "rerun" not in st.session_state:
+    st.session_state.rerun = True
+    st.rerun()
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+file = st.experimental_get_query_params().get('file')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+# df = pd.read_csv('https://test-aliyun8203292432-beijing.oss-cn-beijing.aliyuncs.com/test/data/warehouse_genotype_snp_csv_sample-col_batch_PMBD2023080733P01%20%282%29.csv')
+df = pd.read_excel(file[0])
+builder = GridOptionsBuilder.from_dataframe(df)
+builder.configure_pagination(paginationPageSize=20, paginationAutoPageSize=False)
+go = builder.build()
+with st.container():
+    AgGrid(df, gridOptions=go)
